b2f24b99978a4c56030e0279262580405c328317
Nicholas J <nicholas.a.johns5@gmail.com>
2018-02-15 17:35:30 -0500

    add github releaser

--- ci/do_github_publish.sh
+++ ci/do_github_publish.sh
@@ -16,4 +16,17 @@ if [[ -z "${GITHUB_TOKEN}" ]]; then
     exit 0
 fi
 
+TAG="${CIRCLE_TAG:-}"
+if [[ -z "${TAG:-}" ]]; then
+    TAG=$(git describe --abbrev=0 --tags)
+fi
 
+wget https://github.com/aktau/github-release/releases/download/v0.7.2/linux-amd64-github-release.tar.bz2 -O /tmp/ghrelease.tar.bz2
+tar -xvjpf /tmp/ghrelease.tar.bz2 -C /tmp
+cp /tmp/bin/linux/amd64/github-release /usr/local/bin/ghrelease
+chmod +x /usr/local/bin/ghrelease
+
+if [[ ! -z "${TAG:-}" ]]; then
+    ghrelease release --tag "${TAG:-}" --user taion809 --repo envoy-build-tests --name "envoy release ${TAG:-}"
+    ghrelease upload --tag "${TAG:-}" --user taion809 --repo envoy-build-tests --name "envoy-linux-amd64" --file "${ENVOY_SRCDIR}/build_release_stripped/envoy"
+fi
